97bee58a3aeda810572e709176230c1f0d4a184d
Nitin Ankush Tambe <110184860+tambenitin2002@users.noreply.github.com>
2024-12-04 15:15:25 +0530

    Add files via upload

---
+++ Java With Video/rectangle_patern.java
@@ -0,0 +1,10 @@
+public class rectangle_patern{
+    public static void main(String[] args) {
+        for(int count=1; count<=7;count++){
+            System.out.println();
+            for(int star=1;star<=20;star++){
+                System.out.print("*");
+            }
+        }
+    }
+}
